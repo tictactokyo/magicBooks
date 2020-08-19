a18a83ddcce01744954e3f139af88ee8c5ee30c1
tictactokyo <tictactokyo00@gmail.com>
2020-08-19 10:28:58 -0700

    added reflect code
    movment still not working reflect still not implemented

---
+++ Assets/activeReflect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activeReflect : MonoBehaviour
+{
+
+	public GameObject explosion;
+
+
+
+
+	public int dur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Boundary" || other.tag == "Player")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		//Instantiate(other.gameObject, transform.position, transform.rotation);
+
+
+		Destroy (other.gameObject);
+
+	}
+    // Update is called once per frame
+    void Update()
+    {
+		dur--;
+
+		if(dur<0){
+
+			Destroy (gameObject);
+		}
+    }
+}
