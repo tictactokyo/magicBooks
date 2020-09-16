9d6cd8f6eb75212bf239b0ee547d0c8be53b7306
tictactokyo <tictactokyo00@gmail.com>
2020-09-16 10:50:33 -0700

    added crouching  and STATE TRANSITION
    crouch

--- Assets/PlayerController_FSN.cs
+++ Assets/PlayerController_FSN.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerController_FSN : MonoBehaviour
 {
+
+
+    private PlayerBaseState currentPlayerState;
+
+
+    public readonly PlayerIdleState idleState= new PlayerIdleState();
+
+    public readonly PlayerWalkState WalkState = new PlayerWalkState();
+    public readonly PlayerJumpingState JumpingState = new PlayerJumpingState();
+    public readonly PlayerCrouchingState CrouchingState = new PlayerCrouchingState();
+
+
+
+
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+
+
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        TransitionToState(idleState);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        currentPlayerState.Update(this);
     }
+    public void TransitionToState(PlayerBaseState state)
+    {
+        currentPlayerState = state;
+        currentPlayerState.EnterState(this);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        currentPlayerState.OnCollisionEnter(this);
+    }
+
+
+
+
+
+
 }
